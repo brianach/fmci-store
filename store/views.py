e34dbb09ee86f517a3ff860d72842a674aa72add
brianach <>
2023-12-21 22:30:13 +0000

    added store views and templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Store
 
 # Create your views here.
+
+def store(request):
+    """ A view to return store page """
+
+    store = Store.objects.all()
+
+    context = {
+        'store': store,
+    }
+
+    return render(request, 'store/store.html', context)
