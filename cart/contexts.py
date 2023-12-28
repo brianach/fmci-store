64696ed8b12484fae8185d188f5efb3b336fdeaa
brianach <>
2023-12-28 21:47:14 +0000

    fix: close href in base.html

--- cart/contexts.py
+++ cart/contexts.py
@@ -5,7 +5,7 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    store_count = 0
 
     delivery = 0
     
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def cart(request):
+def view_cart(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'cart/cart.html')
