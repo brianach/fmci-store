b7a9e05fdb2599727198587168d0447a6c8c7958
brianach <>
2024-01-04 10:42:09 +0000

    save before recreating crashing workspace

--- space/views.py
+++ space/views.py
@@ -3,9 +3,8 @@ from .models import Space, Labspace, Deskspace
 
 
 def spaces(request):
+    spaces = Space.objects.all()
     deskspaces = Deskspace.objects.all()
     labspaces = Labspace.objects.all()
-
-    spaces = list(deskspaces) + list(labspaces)
 
     return render(request, 'space/space.html', {'spaces': spaces})
